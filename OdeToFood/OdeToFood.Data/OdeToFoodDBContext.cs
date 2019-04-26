39576b1bf02bacaf490986099e55cbbe8ac52e7e
erinkayb <erinkayb@gmail.com>
2019-04-26 10:29:16 +0200

    Added entity framework to project and set up context.

---
+++ OdeToFood/OdeToFood.Data/OdeToFoodDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    // inherited from entity framework core's dbcontext class
+    public class OdeToFoodDBContext : DbContext
+    {
+        // add properties with the data that needs to be stored in the database
+
+        // DbSet used to tell entity framework to not only query for Restaurant information but also need to insert, update and delete restaurant information.
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
